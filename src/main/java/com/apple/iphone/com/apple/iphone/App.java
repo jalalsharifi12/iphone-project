0df116605ee7a2e2f0c5beb377f1ed4e1433e6fb
Ahmad Jalal Sharifi <jalal.sharifi.12@gmail.com>
2021-11-25 20:51:39 -0800

    iphone project updates

---
+++ src/main/java/com/apple/iphone/com/apple/iphone/App.java
@@ -0,0 +1,13 @@
+package com.apple.iphone.com.apple.iphone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
